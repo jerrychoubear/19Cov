05a6813475f34d94f6c95f0a1c391929495a4aa0
Jchou <jchou@ftftfund.com.tw>
2020-02-24 17:27:05 +0800

    加入專案檔案。

---
+++ 19Cov/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace _19Cov
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                using (var http = new HttpClient())
+                {
+                    Console.Write("取得公開資訊中...");
+                    var req = await http.GetAsync("https://od.cdc.gov.tw/eic/Age_County_Gender_19Cov.json");
+                    if (!req.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine($"失敗！({(int)req.StatusCode}-{req.ReasonPhrase})");
+                        return;
+                    }
+                    
+                    Console.WriteLine("OK!");
+                    var json = await req.Content.ReadAsStringAsync();
+                    var bytes = await req.Content.ReadAsByteArrayAsync();
+
+                    var 確診病例 = JsonSerializer.Deserialize<List<確診>>(json);
+                    var 各縣市確診數 = 確診病例.GroupBy(x => x.縣市)
+                        .Select(g => new
+                        {
+                            縣市 = g.Key,
+                            病例數 = g.Sum(x => int.Parse(x.確定病例數))
+                        });
+                    foreach (var 群組 in 各縣市確診數)
+                    {
+                        Console.WriteLine($"{群組.縣市}： {群組.病例數} 例");
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine($"全台共： {確診病例.Sum(x => Convert.ToInt32(x.確定病例數))} 例");
+
+                    File.WriteAllBytes(@"C:\temp\cov19.txt", bytes);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"失敗！\n錯誤訊息如下：\n{ex}");
+            }
+        }
+    }
+}
---
+++ 19Cov/確診.cs
@@ -0,0 +1,11 @@
+﻿public class 確診
+{
+    public string 確定病名 { get; set; }
+    public string 診斷年份 { get; set; }
+    public string 診斷月份 { get; set; }
+    public string 縣市 { get; set; }
+    public string 性別 { get; set; }
+    public string 是否為境外移入 { get; set; }
+    public string 年齡層 { get; set; }
+    public string 確定病例數 { get; set; }
+}
